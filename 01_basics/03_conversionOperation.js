79b6f787145fccfb8394d89e0d422ad4b2744b62
Dev-Enthusiast01 <148749872+Dev-Enthusiast01@users.noreply.github.com>
2023-10-25 07:07:57 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// this is how we can do conversion i.e = Number data type used.
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = "Suraj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "suraj" => true
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
