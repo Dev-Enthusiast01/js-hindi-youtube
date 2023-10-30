18a26091e577500128b22c1a91d8072522804470
Dev-Enthusiast01 <148749872+Dev-Enthusiast01@users.noreply.github.com>
2023-10-30 09:29:12 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,20 @@
+//Global Scope
+let a = 300
+
+// Local Scope
+if (true) {
+    let a = 10
+    const b = 23
+    console.log("INEER: ",a);
+    
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array [i];
+    
+}
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
