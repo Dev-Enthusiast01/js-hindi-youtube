8c4e1c6739e30e7bfc35241a99d7a12461d6c8ab
Dev-Enthusiast01 <148749872+Dev-Enthusiast01@users.noreply.github.com>
2023-10-31 06:12:10 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -29,4 +29,36 @@ function one(){
 
 }
 
-one()
+// one()
+
+if (true) {
+    const username = "Suraj"
+
+    if (username === "Suraj") {
+        const website = " youtube"
+        //console.log(username + website); //hoga
+    }
+
+    // console.log(website); // nahi hoga 
+    
+}
+
+// console.log(username); //nahi hoga
+
+
+//+++++++++++++++++ Interesting ++++++++++++++++
+
+
+console.log(addone(5));
+
+function addone(num) {
+    return num + 1
+}
+
+
+addTwo(5)
+// js variable can hold fn(), also called as expression
+const addTwo = function (num){
+    return num + 2
+}
+
