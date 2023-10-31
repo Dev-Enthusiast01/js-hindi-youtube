3227eeee7731fd47c7e0082c1e0df7222576b556
Dev-Enthusiast01 <148749872+Dev-Enthusiast01@users.noreply.github.com>
2023-10-31 10:27:30 +0000

    scope

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,8 +1,21 @@
 // if 
 const isUserloggedIn = true
+const temperature = 41
 
-if (2 == "2") {
-   console.log("executed");
+// if (temperature === 41 ) {
+//    console.log("less than 50");
+// } else {
+//    console.log("temperature is greater than 50");
+// }
+
+// console.log("Executed");
+// <, >, <=, >=, ==, !=, ===, !==
+
+const score = 200
+
+if (score > 100) {
+   var power = "fly"
+   console.log(`User power:${power}`);
 }
 
-// <, >, <=, >=, ==, !=, ===
+console.log(`User power:${power}`);
