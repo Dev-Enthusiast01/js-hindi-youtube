380892fd6a63525e0505ca0b196d61c8d623830a
Dev-Enthusiast01 <148749872+Dev-Enthusiast01@users.noreply.github.com>
2023-11-02 10:32:47 +0000

    for each loop

---
+++ five.js
@@ -0,0 +1,9 @@
+// for each loop
+// callbackfn mein naam nahi hota hai 
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val) {
+//     console.log(val);
+// })
+
+coding.forEach( () )
